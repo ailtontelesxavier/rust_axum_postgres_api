ff7dd5c5557992a10e58cec1007b5e500d64408a
Ailton teles xavier <atxshared@gmail.com>
2025-05-14 09:49:32 -0300

    create handler

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use std::sync::Arc;
+
+use axum::{response::IntoResponse, routing::get, Json, Router};
+use dotenv::dotenv;
+
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+
+async fn health_checker_handler() -> impl IntoResponse {
+    const MESSAGE: &str = "Simple CRUD API with Rust, SQLX, Postgres,and Axum";
+
+    let json_response = serde_json::json!({
+        "status": "success",
+        "message": MESSAGE
+    });
+
+    Json(json_response)
+}
+
+#[allow(dead_code)]
+pub struct AppState {
+    db: Pool<Postgres>,
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("✅Connection to the database is successful!");
+            pool
+        }
+        Err(err) => {
+            println!("🔥 Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
+
+    let app_state = Arc::new(AppState { db: pool.clone() });
+    let app = Router::new()
+        .route("/api/healthchecker", get(health_checker_handler))
+        .with_state(app_state);
+
+    println!("🚀 Server started successfully");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8008").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
